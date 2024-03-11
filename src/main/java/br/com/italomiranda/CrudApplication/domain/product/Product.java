621146cb4263b39de9658536ea264e00105e1ba1
Ítalo Miranda Fontenele <113559117+Ital023@users.noreply.github.com>
2024-03-10 21:33:40 -0300

    feat: lombok and product class

--- src/main/java/br/com/italomiranda/CrudApplication/domain/product/Product.java
+++ src/main/java/br/com/italomiranda/CrudApplication/domain/product/Product.java
@@ -1,8 +1,21 @@
 package br.com.italomiranda.CrudApplication.domain.product;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
-@Entity
+@Entity(name = "product")
+@Table(name = "product")
+@EqualsAndHashCode(of = "id")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String name;
+    private Number price_in_cents;
 }
