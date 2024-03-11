2b663857f454e374dd4393276eb084d6130d3dd7
Ítalo Miranda Fontenele <113559117+Ital023@users.noreply.github.com>
2024-03-11 17:21:00 -0300

    feat: PUT METHOD

--- src/main/java/br/com/italomiranda/CrudApplication/controllers/ProductController.java
+++ src/main/java/br/com/italomiranda/CrudApplication/controllers/ProductController.java
@@ -3,10 +3,14 @@ package br.com.italomiranda.CrudApplication.controllers;
 import br.com.italomiranda.CrudApplication.domain.product.Product;
 import br.com.italomiranda.CrudApplication.domain.product.ProductRepository;
 import br.com.italomiranda.CrudApplication.domain.product.RequestProduct;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 
 @RestController
@@ -30,6 +34,18 @@ public class ProductController {
         return ResponseEntity.ok(requestProduct);
     }
 
-
+    @PutMapping
+    @Transactional
+    public ResponseEntity updateProduct(@RequestBody @Valid RequestProduct data){
+        Optional<Product> optionalProduct = productRepository.findById(data.id());
+        if (optionalProduct.isPresent()) {
+            Product product = optionalProduct.get();
+            product.setName(data.name());
+            product.setPrice_in_cents(data.price_in_cents());
+            return ResponseEntity.ok(product);
+        } else {
+            throw new EntityNotFoundException();
+        }
+    }
 
 }
--- src/main/java/br/com/italomiranda/CrudApplication/domain/product/RequestProduct.java
+++ src/main/java/br/com/italomiranda/CrudApplication/domain/product/RequestProduct.java
@@ -3,5 +3,5 @@ package br.com.italomiranda.CrudApplication.domain.product;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public record RequestProduct(@NotBlank String name,@NotNull Integer price_in_cents) {
+public record RequestProduct(String id,@NotBlank String name,@NotNull Integer price_in_cents) {
 }
